a3ac042d53bf413d28940acd729a59611c486f5d
Thomas Scheffler <github@yagee.de>
2016-10-05 13:03:36 +0200

    removed scripts and files that do not work

--- convert-svn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-script=$(basename $0);
-dir=$(dirname $0);
-target="${dir}/git-repos"
-mkdir "${target}"
-git-svn-migrate/git-svn-migrate.sh --url-file=mycore-svn-list.txt --authors-file=authors.txt "${target}"
